b84e5fd9501e533587245e8ba551acf48a4bfaa0
baedollee <baesee0401@gmail.com>
2022-08-14 22:14:01 +0900

    메인 리스트 css 컴포넌트 만드는중

--- src/pages/main/Main.js
+++ src/pages/main/Main.js
@@ -1,23 +1,32 @@
 import React from 'react';
 import { useState } from 'react';
 import MainHeader from './MainHeader';
+import MainList from './MainList';
 
 const Main = () => {
-  const [movieList, setMovieList] = useState({
-    movieTitle: '',
-    movieDate: '',
-  });
+  const [movieList, setMovieList] = useState([
+    {
+      id: 0,
+      movieTitle: '올드보이',
+      movieDate: '22/8/14',
+      movieImage:
+        'https://breed-assets.wisdompanel.com/dog/poodle-miniature-toy/poodle-miniature-toy1.jpg',
+      likeCount: '2',
+    },
+  ]);
   return (
     <div>
       <MainHeader />
-
       <div>
-        <img src="https://breed-assets.wisdompanel.com/dog/poodle-miniature-toy/poodle-miniature-toy1.jpg" />
-        <div>
-          <span>제목</span>
-          <div>하트</div>
-        </div>
-        <div>개봉날짜</div>
+        {movieList.map((item, index) => {
+          return (
+            <MainList
+              key={item.id}
+              movieList={movieList}
+              setMovieList={setMovieList}
+            />
+          );
+        })}
       </div>
     </div>
   );
