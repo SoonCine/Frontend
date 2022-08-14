724ea057d2153fb61ad04ad3e37503cf5d9f4204
Rita Soojin Woo <linawoo@naver.com>
2022-08-14 17:14:03 +0900

    build layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 //Router import
-import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 
 //Component import
-import Detail from './pages/detail/Detail';
-import SignIn from './pages/signin/SignIn';
-import Main from './pages/main/Main';
-import SignUp from './pages/signup/SignUp';
+import Detail from "./pages/detail/Detail";
+import SignIn from "./pages/signin/SignIn";
+import Main from "./pages/main/Main";
+import SignUp from "./pages/signup/SignUp";
 
 function App() {
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path='/' element={<SignIn/>}/>
-      <Route path='/signup' element={<SignUp/>}/>
-      <Route path='/main' element={<Main/>}/>
-      <Route path='/detail' element={<Detail/>}/>
-    </Routes>
+      <Routes>
+        <Route path="/" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/main" element={<Main />} />
+        <Route path="/detail" element={<Detail />} />
+      </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
 export default App;
--- src/pages/detail/Detail.js
+++ src/pages/detail/Detail.js
@@ -1,7 +1,61 @@
 import React from "react";
+import {
+  WholeDetail,
+  ImageNinfo,
+  ImageBox,
+  MovieImage,
+  MovieInfo,
+  MovieTitle,
+  MovieEngtitle,
+  MovieGenre,
+  Likes,
+  CommentBox,
+  CommentNbutton,
+  InputComment,
+  SaveButton,
+  CommentList,
+  IndivComment,
+  CommentNickname,
+  CommentContent,
+  DeleteButton,
+} from "./DetailStyled";
 
-const Detail = () => {
-    return <div>Detail</div>
+function Detail() {
+  return (
+    <WholeDetail>
+      <ImageNinfo>
+        <ImageBox>
+          이미지 들어갈 자리
+          <MovieImage src="https://breed-assets.wisdompanel.com/dog/poodle-miniature-toy/poodle-miniature-toy1.jpg" />
+        </ImageBox>
+        <MovieInfo>
+          <MovieTitle>영화 제목</MovieTitle>
+          <MovieEngtitle>영어 제목</MovieEngtitle>
+          <MovieGenre>장르</MovieGenre>
+          <Likes> ♥ </Likes>
+        </MovieInfo>
+      </ImageNinfo>
+      <CommentBox>
+        <CommentNbutton>
+          <InputComment
+            type="text"
+            placeholder="댓글을 입력해 주세요."
+          ></InputComment>
+          <SaveButton>등록</SaveButton>
+        </CommentNbutton>
+        <hr></hr>
+        <div>
+          <CommentList>
+            <IndivComment>
+              <CommentNickname> 닉네임 </CommentNickname>
+              <CommentContent> 댓글 내용 </CommentContent>
+              <DeleteButton>🗑️</DeleteButton>
+            </IndivComment>
+          </CommentList>
+        </div>
+      </CommentBox>
+    </WholeDetail>
+  );
 }
 
 export default Detail;
