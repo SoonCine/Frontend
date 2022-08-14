01ffaaa1c83178b5c655df400d570fe9588c8b97
baedollee <baesee0401@gmail.com>
2022-08-14 21:21:10 +0900

    css 작업중입니다.

--- src/pages/main/Main.js
+++ src/pages/main/Main.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import { useState } from 'react';
+import MainHeader from './MainHeader';
 
 const Main = () => {
-  return <div>Main</div>;
+  const [movieList, setMovieList] = useState({
+    movieTitle: '',
+    movieDate: '',
+  });
+  return (
+    <div>
+      <MainHeader />
+
+      <div>
+        <img src="https://breed-assets.wisdompanel.com/dog/poodle-miniature-toy/poodle-miniature-toy1.jpg" />
+        <div>
+          <span>제목</span>
+          <div>하트</div>
+        </div>
+        <div>개봉날짜</div>
+      </div>
+    </div>
+  );
 };
 
 export default Main;
