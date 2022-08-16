793956d832f4f002ca39ef7f00463311de730400
baedollee <baesee0401@gmail.com>
2022-08-17 00:10:35 +0900

    주석 삭제

--- src/pages/main/Main.js
+++ src/pages/main/Main.js
@@ -1,11 +1,11 @@
-import React from "react";
-import MainHeader from "../../component/main/MainHeader";
-import MainList from "../../component/main/MainList";
-import MainNav from "../../component/main/MainNav";
-import { MainWrap, MainListContainer } from "./MainStyled";
-import { useSelector, useDispatch } from "react-redux";
-import { useEffect } from "react";
-import { asyncGetMovieList } from "../../redux/modules/movieList";
+import React from 'react';
+import MainHeader from '../../component/main/MainHeader';
+import MainList from '../../component/main/MainList';
+import MainNav from '../../component/main/MainNav';
+import { MainWrap, MainListContainer } from './MainStyled';
+import { useSelector, useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { asyncGetMovieList } from '../../redux/modules/movieList';
 
 const Main = () => {
   const dispatch = useDispatch();
@@ -17,11 +17,6 @@ const Main = () => {
     dispatch(asyncGetMovieList());
   }, []);
 
-  // useEffect(() => {
-  //   axios.get('http://54.180.89.34:8080/api/movie/upcomming').then((res) => {
-  //     setList(res.data.data.slice(0,12));
-  //   });
-  // }, []);
   return (
     <MainWrap>
       <MainHeader />
